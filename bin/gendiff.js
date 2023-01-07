2373420afc13c6ec0cb518fa58d9d13d81a6af79
polinagv <pol.grachewa@yandex.ru>
2023-01-08 00:36:38 +0400

    step 2: added arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,10 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference')
-  .version('0.8.0');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
